5a32e64086cd1bbd4119472a47686a424e351283
Gustavo Augusto Reginatto <60835063+Borgil-An@users.noreply.github.com>
2023-06-18 16:38:28 -0300

    Update Exercicio01.c

--- Exercicio01.c
+++ Exercicio01.c
@@ -1,3 +1,9 @@
+//Escreva um programa que:
+//(a) Crie/abra um arquivo texto de nome “arq.txt”
+//(b) Permita que o usuario grave diversos caracteres nesse arquivo, até que o usuário entre com o caractere ‘0’
+//(c) Feche o arquivo
+//Agora, abra e leia o arquivo, caractere por caractere, e escreva na tela todos os caracteres armazenados.
+
 // Resolução da primeira parte do exercício, linhas 8,11,12,13 dedicadas á abertura do arquivo e a sua verificação, linhas 16 á 21 dedicadas a resolução do que é proposto em b e c.
 
 #include <stdio.h>
@@ -19,5 +25,19 @@ int main (void){
         fprintf (fo,"%c",c);
     }
     fclose (fo);
+}
+
+//Modificação do código necessária para abrir o arquivo e ler o mesmo.
+
+ fo = fopen ("arq.txt","r");
+    if (fo == NULL){
+        return 0; 
+    }
+
+  while(!feof(fo)){
+    fscanf (fo,"%c",&c);
+    printf ("%c",c);
+  }
+fclose(fo);
 }
 
